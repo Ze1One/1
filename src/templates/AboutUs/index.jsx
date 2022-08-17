40556a87ac0ce0bb643d589dfac6bf182815b4fd
Ze1One <103776420+Ze1One@users.noreply.github.com>
2022-08-17 15:30:57 +0300

    1

--- src/templates/AboutUs/index.jsx
+++ src/templates/AboutUs/index.jsx
@@ -1,4 +1,19 @@
-import React, { useState } from "react";
-import AboutUs from "../AboutUs";
+import React from 'react';
+// import AboutUs from "../AboutUs";
+
+import './style.scss';
+
+const AboutUs = (props) => {
+    const { title, subtitle } = props;
+    return (
+        <div className='about'>
+            <h1 className='about__title'>{title}</h1>
+            <div>
+                <p className='about__content'>{subtitle}</p>>
+            </div>
+        </div>
+
+    )
+};
 
 export default AboutUs;
